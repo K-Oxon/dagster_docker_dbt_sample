983720712c2e74ea7cf7dcba30a9cff44755cf38
Ken Okumura <kokumura@yahoo-corp.jp>
2023-08-27 20:18:33 +0900

    add dagster dbt integration

--- repo.py
+++ repo.py
@@ -1,40 +1,40 @@
-from dagster import FilesystemIOManager, graph, op, repository, schedule
-from dagster_docker import docker_executor
+import os
+from pathlib import Path
 
+from dagster import Definitions, OpExecutionContext, ScheduleDefinition
+from dagster_dbt import DbtCliResource, build_schedule_from_dbt_selection, dbt_assets
 
-@op
-def hello():
-    return 1
+dbt_project_dir = Path(__file__).joinpath("..", "jaffle_shop").resolve()
 
-
-@op
-def goodbye(foo):
-    if foo != 1:
-        raise Exception("Bad io manager")
-    return foo * 2
-
-
-@graph
-def my_graph():
-    goodbye(hello())
-
-
-my_job = my_graph.to_job(name="my_job")
-
-my_step_isolated_job = my_graph.to_job(
-    name="my_step_isolated_job",
-    executor_def=docker_executor,
-    resource_defs={
-        "io_manager": FilesystemIOManager(base_dir="/tmp/io_manager_storage")
-    },
+dbt = DbtCliResource(
+    project_dir=os.fspath(dbt_project_dir), profiles_dir=os.fspath(dbt_project_dir)
 )
 
+# If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
+# Otherwise, we expect a manifest to be present in the project's target directory.
+if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+    dbt_parse_invocation = dbt.cli(["parse"], manifest={}).wait()
+    dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+else:
+    dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
 
-@schedule(cron_schedule="* * * * *", job=my_job, execution_timezone="US/Central")
-def my_schedule(_context):
-    return {}
+
+@dbt_assets(manifest=dbt_manifest_path)
+def jaffle_shop_dbt_assets(context: OpExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
 
 
-@repository
-def deploy_docker_repository():
-    return [my_job, my_step_isolated_job, my_schedule]
+daily_dbt_assets_schedule = build_schedule_from_dbt_selection(
+    [jaffle_shop_dbt_assets],
+    job_name="all_assets_daily_job",
+    cron_schedule="0 0 * * *",
+    dbt_select="fqn:*",
+)
+
+defs = Definitions(
+    assets=[jaffle_shop_dbt_assets],
+    schedules=[daily_dbt_assets_schedule],
+    resources={
+        "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+    },
+)
